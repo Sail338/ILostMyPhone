bfbe433f4376ef0185344993c520b487261b01cf
Sail338 <srihari.shankar1@gmail.com>
2018-09-30 06:26:29 -0400

    almost there

--- server/app.py
+++ server/app.py
@@ -4,12 +4,31 @@ from funk import three_circle
 from sanic import Sanic
 from sanic.response import json
 app = Sanic()
-
+address_map = dict()
 @app.route('/read_points', methods = ['POST'])
-async def test(request):
+def test(request):
 #    print(request.body.address)    
     print(request.json)
+    json_ = request.json
+    if json_['address'] not in address_map:
+        print("not there")
+        address_map[json_['address']] = dict()
+    tuple_map = address_map[json_['address']]
+    id_ = json_['id']
+    rssi = json_['rssi']
+    if id_  not in tuple_map:
+        tuple_map[id_] = rssi
+        print(tuple_map)
+        if len(tuple_map) == 3:
+             a =three_circle.get_user_location_by_response(tuple_map['a'],tuple_map['b'],tuple_map['c'])
+             print(a)
+             tuple_map = {}
+             address_map[json_['address']] = {}
     return json({'hello': 'world'})
+
+
+
+
 
 if __name__ == '__main__':
         app.run(port=5000)
