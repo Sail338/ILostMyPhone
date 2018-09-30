02931f06b285265aa370069467bf406e7b27bae3
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-29 22:21:35 -0400

    fixed shit

--- server/funk/three_circle.py
+++ server/funk/three_circle.py
@@ -6,7 +6,7 @@ def three_circle_intersection(x0, y0, r0, x1, y1, r1, x2, y2, r2):
     d = 1
     j = 1
     x = ((r0**2) - (r1**2) + (d**2)) / (d*2)
-    y = (((r0**2) - (r2**2) + (i**2) + (j**2)) / ((2*j) - (i*x))) / j
+    y = (((r0**2) - (r2**2) + (i**2) + (j**2)) / (2*j)) - ((i*x) / j)
     return (x,y)
 
 def get_user_location_by_response(rssia, rssib, rssic):
