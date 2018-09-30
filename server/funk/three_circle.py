39a5914ab56779f566852af7a5a61b3240484d3c
Rangaraj Tirumala <rangaraj.t@gmail.com>
2018-09-29 21:15:59 -0400

    did some math

---
+++ server/funk/three_circle.py
@@ -0,0 +1,13 @@
+import math
+
+def three_circle_intersection(x0, y0, r0, x1, y1, r1, x2, y2, r2):
+    # skipped some shit
+    i = math.sqrt(2)
+    d = 1
+    j = 1
+    x = ((r0**2) - (r1**2) + (d**2)) / (d*2)
+    y = (((r0**2) - (r2**2) + (i**2) + (j**2)) / ((2*j) - (i*x))) / j
+    return (x,y)
+
+def get_user_location_by_response(rssia, rssib, rssic):
+    return three_circle_intersection(0, 0, abs(rssia), 1, 0, abs(rssib), 0, 1, abs(rssic))
